37905947f28a2cf8060b633f9f38246896df11c4
Danh007 <denbeig70@gmail.com>
2026-05-27 09:09:49 +0300

    Force formating for a phonenumber

--- javascript/login.js
+++ javascript/login.js
@@ -12,3 +12,17 @@ function removeNumbers(event) {
 function removeLetters(event) {
     event.target.value = event.target.value.replace(/\D/g, '');
 }
+
+function formatPhoneNumber(event) {
+    let num = event.target.value.replace(/\D/g, '');
+
+    if (num.length <= 3) {
+        event.target.value = num;
+    } 
+    else if (num.length <= 6) {
+        event.target.value = `${num.slice(0, 3)} ${num.slice(3)}`;
+    } 
+    else {
+        event.target.value = `${num.slice(0, 3)} ${num.slice(3, 6)}-${num.slice(6, 10)}`;
+    }
+}
